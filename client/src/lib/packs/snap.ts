84ad004209a973c31322829507f137a77e96e582
oignonmetro <roadtokhagne@gmail.com>
2026-09-02 11:11:01 +0200

    Ajoute des templates au pack Snap francais

--- client/src/lib/packs/snap.ts
+++ client/src/lib/packs/snap.ts
@@ -19,4 +19,160 @@ import type { Template } from '../../types';
 //
 // Tant que ce tableau est vide, le pack n'est pas enregistré du tout dans
 // packs/index.ts : il n'apparaît ni dans le lobby, ni dans les tests.
-export const SNAP_TEMPLATES: Template[] = [];
+export const SNAP_TEMPLATES: Template[] = [
+  {
+    id: 'snap-8-morts-6-blesses-je-pete-ma-biere-ma-libellule-thumbnail',
+    url: '/templates/snap-8-morts-6-blesses-je-pete-ma-biere-ma-libellule-thumbnail.jpg',
+    name: '8 morts 6 blessés je pète ma bière ma libellule thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 12, widthPct: 90, heightPct: 20 }],
+  },
+  {
+    id: 'snap-ah-batard-tu-fumes-lacrim-thumbnail',
+    url: '/templates/snap-ah-batard-tu-fumes-lacrim-thumbnail.jpg',
+    name: 'Ah batard tu fumes lacrim thumbnail',
+    source: 'library',
+    boxes: [
+      { xPct: 50, yPct: 9, widthPct: 90, heightPct: 13 },
+      { xPct: 50, yPct: 93, widthPct: 90, heightPct: 15 },
+    ],
+  },
+  {
+    id: 'snap-b032sf',
+    url: '/templates/snap-b032sf.jpg',
+    name: 'B032sf',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 15, widthPct: 90, heightPct: 26 }],
+  },
+  {
+    id: 'snap-cadenas',
+    url: '/templates/snap-cadenas.jpg',
+    name: 'Cadenas',
+    source: 'library',
+    boxes: [
+      { xPct: 48, yPct: 17, widthPct: 30, heightPct: 9 },
+      { xPct: 35, yPct: 45, widthPct: 44, heightPct: 20 },
+    ],
+  },
+  {
+    id: 'snap-cest-reparti-comme-en-46-rohff-thumbnail',
+    url: '/templates/snap-cest-reparti-comme-en-46-rohff-thumbnail.jpg',
+    name: 'Cest reparti comme en 46 rohff thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 7, widthPct: 90, heightPct: 11 }],
+  },
+  {
+    id: 'snap-chante-skype-thumbnail',
+    url: '/templates/snap-chante-skype-thumbnail.jpg',
+    name: 'Chante skype thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 10, widthPct: 90, heightPct: 15 }],
+  },
+  {
+    id: 'snap-excusez-moi-chef-chauffeur-taxi-sourd-thumbnail',
+    url: '/templates/snap-excusez-moi-chef-chauffeur-taxi-sourd-thumbnail.jpg',
+    name: 'Excusez moi chef chauffeur taxi sourd thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 47, yPct: 7, widthPct: 56, heightPct: 12 }],
+  },
+  {
+    id: 'snap-fraise-framboise-myrtille-on-va-bien-renta-le-gateau-thumbnail',
+    url: '/templates/snap-fraise-framboise-myrtille-on-va-bien-renta-le-gateau-thumbnail.jpg',
+    name: 'Fraise framboise myrtille on va bien renta le gâteau thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 68, yPct: 33, widthPct: 54, heightPct: 14 }],
+  },
+  {
+    id: 'snap-il-abuse-de-lheure-le-camion-de-glace-thumbnail',
+    url: '/templates/snap-il-abuse-de-lheure-le-camion-de-glace-thumbnail.jpg',
+    name: 'Il abuse de lheure le camion de glace thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 7, widthPct: 90, heightPct: 9 }],
+  },
+  {
+    id: 'snap-il-digere-rires-snapchat-thumbnail',
+    url: '/templates/snap-il-digere-rires-snapchat-thumbnail.jpg',
+    name: 'Il digère rires snapchat thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 33, widthPct: 50, heightPct: 13 }],
+  },
+  {
+    id: 'snap-la-question-est-vite-repondue-thumbnail',
+    url: '/templates/snap-la-question-est-vite-repondue-thumbnail.jpg',
+    name: 'La question est vite répondue thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 8, widthPct: 90, heightPct: 13 }],
+  },
+  {
+    id: 'snap-le-tunnel-de-dingue-infini-ca-me-fait-peur-thumbnail',
+    url: '/templates/snap-le-tunnel-de-dingue-infini-ca-me-fait-peur-thumbnail.jpg',
+    name: 'Le tunnel de dingue infini ca me fait peur thumbnail',
+    source: 'library',
+    boxes: [
+      { xPct: 50, yPct: 11, widthPct: 90, heightPct: 17 },
+      { xPct: 51, yPct: 70, widthPct: 70, heightPct: 18 },
+    ],
+  },
+  {
+    id: 'snap-mecano',
+    url: '/templates/snap-mecano.jpg',
+    name: 'Mécano',
+    source: 'library',
+    boxes: [{ xPct: 64, yPct: 39, widthPct: 67, heightPct: 16 }],
+  },
+  {
+    id: 'snap-oardefault',
+    url: '/templates/snap-oardefault.jpg',
+    name: 'Oardefault',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 12, widthPct: 90, heightPct: 21 }],
+  },
+  {
+    id: 'snap-pabg-dn3jli7kvvj',
+    url: '/templates/snap-pabg-dn3jli7kvvj.jpg',
+    name: 'Pabg Dn3jlI7KvvJ',
+    source: 'library',
+    boxes: [{ xPct: 80, yPct: 11, widthPct: 42, heightPct: 13 }],
+  },
+  {
+    id: 'snap-tiens-tiens-tiens-booba-thumbnail',
+    url: '/templates/snap-tiens-tiens-tiens-booba-thumbnail.jpg',
+    name: 'Tiens tiens tiens booba thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 50, yPct: 7, widthPct: 90, heightPct: 15 }],
+  },
+  {
+    id: 'snap-tinvite-les-gens-chez-toi-ils-te-terminent-comme-ca-thumbnail',
+    url: '/templates/snap-tinvite-les-gens-chez-toi-ils-te-terminent-comme-ca-thumbnail.jpg',
+    name: 'Tinvite les gens chez toi ils te terminent comme ça thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 51, yPct: 10, widthPct: 92, heightPct: 15 }],
+  },
+  {
+    id: 'snap-tete-avec-un-keuf-thumbnail',
+    url: '/templates/snap-tete-avec-un-keuf-thumbnail.jpg',
+    name: 'Tête avec un keuf thumbnail',
+    source: 'library',
+    boxes: [{ xPct: 51, yPct: 34, widthPct: 90, heightPct: 12 }],
+  },
+  {
+    id: 'snap-w9vyyhyr4x15bqgw',
+    url: '/templates/snap-w9vyyhyr4x15bqgw.jpg',
+    name: 'W9vyyhyr4X15bQgw',
+    source: 'library',
+    boxes: [
+      { xPct: 46, yPct: 40, widthPct: 20, heightPct: 12 },
+      { xPct: 84, yPct: 33, widthPct: 34, heightPct: 23 },
+    ],
+  },
+  {
+    id: 'snap-y-a-heja-entre-2-blaireaux-thumbnail',
+    url: '/templates/snap-y-a-heja-entre-2-blaireaux-thumbnail.jpg',
+    name: 'Y a heja entre 2 blaireaux thumbnail',
+    source: 'library',
+    boxes: [
+      { xPct: 17, yPct: 13, widthPct: 24, heightPct: 14 },
+      { xPct: 45, yPct: 52, widthPct: 27, heightPct: 20 },
+    ],
+  },
+];
